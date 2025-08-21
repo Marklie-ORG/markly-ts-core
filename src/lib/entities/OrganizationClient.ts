5c3935b28e939885491f6cb066af22a006aef865
romapchel <notpchelintsevroma@gmail.com>
2025-08-21 13:47:36 +0300

    delete client filter 1.4.45

--- src/lib/entities/OrganizationClient.ts
+++ src/lib/entities/OrganizationClient.ts
@@ -11,7 +11,7 @@ import { SchedulingOption } from "./SchedulingOption.js";
 import { ClientAdAccount } from "./ClientAdAccount.js";
 import { CommunicationChannel } from "./ClientCommunicationChannel.js";
 
-@Filter({ name: 'softDelete', cond: { deletedAt: null } })
+@Filter({ name: 'softDelete', cond: { deletedAt: null }, default: true })
 @Entity()
 export class OrganizationClient extends BaseEntity {
   @Property()
