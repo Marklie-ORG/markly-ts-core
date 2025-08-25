8fd5a1b81d50ef38e908356963fcf918975b7246
romapchel <notpchelintsevroma@gmail.com>
2025-08-25 17:04:25 +0300

    enabled 1.4.50

--- src/lib/interfaces/ReportsInterfaces.ts
+++ src/lib/interfaces/ReportsInterfaces.ts
@@ -60,6 +60,7 @@ export interface ReportData {
 export interface ReportDataSection {
   name: 'kpis' | 'graphs' | 'ads' | 'campaigns';
   order: number;
+  enabled: boolean;
   adAccounts: ReportDataSectionAdAccount[];
 }
 
@@ -67,6 +68,7 @@ export interface ReportDataSectionAdAccount {
   adAccountId: string;
   order: number;
   adAccountName: string
+  enabled: boolean;
   data: Metric[] | ReportDataAd[] | ReportDataCampaign[] | ReportDataGraph[]
   currency: string
 }
